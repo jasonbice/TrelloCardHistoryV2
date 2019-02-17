a4100f97def72c353225260bd16e59dcaa0f37b4
Jason Bice <jason.bice@gmail.com>
2019-02-17 13:08:32 -0500

    Adding Trello service shell/PoC

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HistoryContainerComponent } from './history/history-container/history-container.component';
@@ -16,6 +16,7 @@ import { HistoryItemComponent } from './history/history-item/history-item.compon
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     RouterModule.forRoot([
       { path: '**', component: HistoryContainerComponent }
     ])
