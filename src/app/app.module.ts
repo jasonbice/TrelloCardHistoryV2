7d346a43a8c8714dc08048d1f7f58999f54c676c
Jason Bice <jason.bice@gmail.com>
2019-02-17 11:37:58 -0500

    Added Trello interfaces

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HistoryContainerComponent } from './history/history-container/history-container.component';
+import { RouterModule } from '@angular/router';
+import { HistoryItemComponent } from './history/history-item/history-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HistoryContainerComponent,
+    HistoryItemComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: '**', component: HistoryContainerComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
