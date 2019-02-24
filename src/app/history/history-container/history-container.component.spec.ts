29d3fe6e50c569511fa008e91c93c069ee58b0d4
Jason Bice <jason.bice@gmail.com>
2019-02-24 11:42:30 -0500

    Adding tests for TrelloDataService

--- src/app/history/history-container/history-container.component.spec.ts
+++ src/app/history/history-container/history-container.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed, fakeAsync, tick, discardPeriodicTasks, flush, flushMicrotasks } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 import { HistoryContainerComponent } from './history-container.component';
 import { HistoryItemListComponent } from '../history-item-list/history-item-list.component';
@@ -7,18 +7,12 @@ import { StringTruncatePipe } from 'src/app/shared/pipes/string-truncate.pipe';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from 'src/app/app-routing.module';
 import chrome from 'sinon-chrome';
-import { ITrelloHistoryDataObj } from 'src/app/shared/models/trello/trello-history-data-obj.model';
-import { History } from 'src/app/shared/models/history/history.model';
 import { TrelloDataService } from 'src/app/services/trello-data.service';
 import { of } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
 import { LegacyCoreService } from 'src/app/services/legacy-core.service';
 import { SortBy, HistoryItem } from 'src/app/shared/models/history/history-item.model';
-
-const MOCK_CARD_JSON: string = '[{"id":"5c7182b7f60f6e4992bcac81","idMemberCreator":"54fce1401bf86d141f6d4326","data":{"list":{"name":"Backlog","id":"5a24378873c3fb5f54030d0f"},"board":{"shortLink":"olZWuQJ6","name":"Trello Card History Development","id":"5a24376ecf0da9d971048058"},"card":{"shortLink":"UEJpVbdt","idShort":32,"name":"(5) Mock Card - Updated Title","id":"5c7172038b60f1763599cc07","desc":"Description changed to remove Lorem Ipsum"},"old":{"desc":"Description changed to update Lorem Ipsum  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}},"type":"updateCard","date":"2019-02-23T17:28:23.427Z","limits":{},"memberCreator":{"id":"54fce1401bf86d141f6d4326","avatarHash":"bf043662d540a9c0e405e4df1f30479e","avatarUrl":"https://trello-avatars.s3.amazonaws.com/bf043662d540a9c0e405e4df1f30479e","fullName":"Jason Bice","idMemberReferrer":null,"initials":"JB","nonPublic":{},"username":"jasonbice2"}},{"id":"5c7182ae75c50a3b9746fd5f","idMemberCreator":"54fce1401bf86d141f6d4326","data":{"list":{"name":"Backlog","id":"5a24378873c3fb5f54030d0f"},"board":{"shortLink":"olZWuQJ6","name":"Trello Card History Development","id":"5a24376ecf0da9d971048058"},"card":{"shortLink":"UEJpVbdt","idShort":32,"name":"(5) Mock Card - Updated Title","id":"5c7172038b60f1763599cc07","desc":"Description changed to update Lorem Ipsum  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."},"old":{"desc":"Description changed to add Lorem Ipsum  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}},"type":"updateCard","date":"2019-02-23T17:28:14.480Z","limits":{},"memberCreator":{"id":"54fce1401bf86d141f6d4326","avatarHash":"bf043662d540a9c0e405e4df1f30479e","avatarUrl":"https://trello-avatars.s3.amazonaws.com/bf043662d540a9c0e405e4df1f30479e","fullName":"Jason Bice","idMemberReferrer":null,"initials":"JB","nonPublic":{},"username":"jasonbice2"}},{"id":"5c71822b123d682929779bad","idMemberCreator":"54fce1401bf86d141f6d4326","data":{"list":{"name":"Backlog","id":"5a24378873c3fb5f54030d0f"},"board":{"shortLink":"olZWuQJ6","name":"Trello Card History Development","id":"5a24376ecf0da9d971048058"},"card":{"shortLink":"UEJpVbdt","idShort":32,"name":"(5) Mock Card - Updated Title","id":"5c7172038b60f1763599cc07","desc":"Description changed to add Lorem Ipsum  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."},"old":{"desc":"Description changed"}},"type":"updateCard","date":"2019-02-23T17:26:03.502Z","limits":{},"memberCreator":{"id":"54fce1401bf86d141f6d4326","avatarHash":"bf043662d540a9c0e405e4df1f30479e","avatarUrl":"https://trello-avatars.s3.amazonaws.com/bf043662d540a9c0e405e4df1f30479e","fullName":"Jason Bice","idMemberReferrer":null,"initials":"JB","nonPublic":{},"username":"jasonbice2"}},{"id":"5c71722d9ade35281cea4722","idMemberCreator":"54fce1401bf86d141f6d4326","data":{"list":{"name":"Backlog","id":"5a24378873c3fb5f54030d0f"},"board":{"shortLink":"olZWuQJ6","name":"Trello Card History Development","id":"5a24376ecf0da9d971048058"},"card":{"shortLink":"UEJpVbdt","idShort":32,"id":"5c7172038b60f1763599cc07","name":"(5) Mock Card - Updated Title"},"old":{"name":"(3) Mock Card - Updated Title"}},"type":"updateCard","date":"2019-02-23T16:17:49.328Z","limits":{},"memberCreator":{"id":"54fce1401bf86d141f6d4326","avatarHash":"bf043662d540a9c0e405e4df1f30479e","avatarUrl":"https://trello-avatars.s3.amazonaws.com/bf043662d540a9c0e405e4df1f30479e","fullName":"Jason Bice","idMemberReferrer":null,"initials":"JB","nonPublic":{},"username":"jasonbice2"}},{"id":"5c71722a3722b80eb07f2947","idMemberCreator":"54fce1401bf86d141f6d4326","data":{"list":{"name":"Backlog","id":"5a24378873c3fb5f54030d0f"},"board":{"shortLink":"olZWuQJ6","name":"Trello Card History Development","id":"5a24376ecf0da9d971048058"},"card":{"shortLink":"UEJpVbdt","idShort":32,"id":"5c7172038b60f1763599cc07","name":"(3) Mock Card - Updated Title"},"old":{"name":"Mock Card - Updated Title"}},"type":"updateCard","date":"2019-02-23T16:17:46.878Z","limits":{},"memberCreator":{"id":"54fce1401bf86d141f6d4326","avatarHash":"bf043662d540a9c0e405e4df1f30479e","avatarUrl":"https://trello-avatars.s3.amazonaws.com/bf043662d540a9c0e405e4df1f30479e","fullName":"Jason Bice","idMemberReferrer":null,"initials":"JB","nonPublic":{},"username":"jasonbice2"}},{"id":"5c71722270819e2c93724abc","idMemberCreator":"54fce1401bf86d141f6d4326","data":{"list":{"name":"Backlog","id":"5a24378873c3fb5f54030d0f"},"board":{"shortLink":"olZWuQJ6","name":"Trello Card History Development","id":"5a24376ecf0da9d971048058"},"card":{"shortLink":"UEJpVbdt","idShort":32,"name":"Mock Card - Updated Title","id":"5c7172038b60f1763599cc07","desc":"Description changed"},"old":{"desc":"Description added"}},"type":"updateCard","date":"2019-02-23T16:17:38.043Z","limits":{},"memberCreator":{"id":"54fce1401bf86d141f6d4326","avatarHash":"bf043662d540a9c0e405e4df1f30479e","avatarUrl":"https://trello-avatars.s3.amazonaws.com/bf043662d540a9c0e405e4df1f30479e","fullName":"Jason Bice","idMemberReferrer":null,"initials":"JB","nonPublic":{},"username":"jasonbice2"}},{"id":"5c71721bee9cc526c722ab31","idMemberCreator":"54fce1401bf86d141f6d4326","data":{"list":{"name":"Backlog","id":"5a24378873c3fb5f54030d0f"},"board":{"shortLink":"olZWuQJ6","name":"Trello Card History Development","id":"5a24376ecf0da9d971048058"},"card":{"shortLink":"UEJpVbdt","idShort":32,"name":"Mock Card - Updated Title","id":"5c7172038b60f1763599cc07","desc":"Description added"},"old":{"desc":""}},"type":"updateCard","date":"2019-02-23T16:17:31.689Z","limits":{},"memberCreator":{"id":"54fce1401bf86d141f6d4326","avatarHash":"bf043662d540a9c0e405e4df1f30479e","avatarUrl":"https://trello-avatars.s3.amazonaws.com/bf043662d540a9c0e405e4df1f30479e","fullName":"Jason Bice","idMemberReferrer":null,"initials":"JB","nonPublic":{},"username":"jasonbice2"}},{"id":"5c7172123a502b11a742a932","idMemberCreator":"54fce1401bf86d141f6d4326","data":{"list":{"name":"Backlog","id":"5a24378873c3fb5f54030d0f"},"board":{"shortLink":"olZWuQJ6","name":"Trello Card History Development","id":"5a24376ecf0da9d971048058"},"card":{"shortLink":"UEJpVbdt","idShort":32,"id":"5c7172038b60f1763599cc07","name":"Mock Card - Updated Title"},"old":{"name":"Mock Card"}},"type":"updateCard","date":"2019-02-23T16:17:22.180Z","limits":{},"memberCreator":{"id":"54fce1401bf86d141f6d4326","avatarHash":"bf043662d540a9c0e405e4df1f30479e","avatarUrl":"https://trello-avatars.s3.amazonaws.com/bf043662d540a9c0e405e4df1f30479e","fullName":"Jason Bice","idMemberReferrer":null,"initials":"JB","nonPublic":{},"username":"jasonbice2"}},{"id":"5c7172038b60f1763599cc08","idMemberCreator":"54fce1401bf86d141f6d4326","data":{"board":{"shortLink":"olZWuQJ6","name":"Trello Card History Development","id":"5a24376ecf0da9d971048058"},"list":{"name":"Backlog","id":"5a24378873c3fb5f54030d0f"},"card":{"shortLink":"UEJpVbdt","idShort":32,"name":"Mock Card","id":"5c7172038b60f1763599cc07"}},"type":"createCard","date":"2019-02-23T16:17:07.324Z","limits":{},"memberCreator":{"id":"54fce1401bf86d141f6d4326","avatarHash":"bf043662d540a9c0e405e4df1f30479e","avatarUrl":"https://trello-avatars.s3.amazonaws.com/bf043662d540a9c0e405e4df1f30479e","fullName":"Jason Bice","idMemberReferrer":null,"initials":"JB","nonPublic":{},"username":"jasonbice2"}}]'
-const MOCK_TRELLO_HISTORY_DATA_OBJS: ITrelloHistoryDataObj[] = JSON.parse(MOCK_CARD_JSON);
-const MOCK_SHORT_LINK: string = 'olZWuQJ6';
-const MOCK_HISTORY: History = new History('olZWuQJ6', MOCK_TRELLO_HISTORY_DATA_OBJS);
+import { HistoryMock } from 'src/app/shared/models/history/history.model.mock';
 
 describe('HistoryContainerComponent', () => {
   let component: HistoryContainerComponent;
@@ -86,11 +80,11 @@ describe('HistoryContainerComponent', () => {
       trelloDataService = TestBed.get(TrelloDataService);
 
       let qShortLink: any = new Promise((resolve, reject) => {
-        resolve(MOCK_SHORT_LINK);
+        resolve(HistoryMock.MOCK_SHORT_LINK);
       });
 
       spyOn(coreService, 'getTrelloCardIdFromCurrentUrl').and.returnValue(qShortLink);
-      spyOn(trelloDataService, 'getHistory').and.returnValue(of(MOCK_HISTORY));
+      spyOn(trelloDataService, 'getHistory').and.returnValue(of(HistoryMock.MOCK_HISTORY));
     });
 
     it('should set the title', async(() => {
@@ -100,7 +94,7 @@ describe('HistoryContainerComponent', () => {
         const titleElement: HTMLElement = fixture.nativeElement.querySelector('h6');
 
         const actual = titleElement.innerHTML.trim();
-        const expected = MOCK_HISTORY.title;
+        const expected = HistoryMock.MOCK_HISTORY.title;
 
         expect(actual).toBe(expected);
       });
@@ -174,7 +168,7 @@ describe('HistoryContainerComponent', () => {
         coreService.isRunningInExtensionMode = true;
 
         component.loadHistory().then(() => {
-          expect(component.shortLink).toBe(MOCK_SHORT_LINK);
+          expect(component.shortLink).toBe(HistoryMock.MOCK_SHORT_LINK);
         });
       }));
 
@@ -182,7 +176,7 @@ describe('HistoryContainerComponent', () => {
         coreService.isRunningInExtensionMode = true;
 
         component.loadHistory().then(() => {
-          expect(component.history).toBe(MOCK_HISTORY);
+          expect(component.history).toBe(HistoryMock.MOCK_HISTORY);
         });
       }));
 
@@ -200,7 +194,7 @@ describe('HistoryContainerComponent', () => {
         spyOn(component.historyItemFilter, 'filter').and.callThrough();
         spyOn(HistoryItem, 'sort').and.callThrough();
 
-        console.log("test", MOCK_HISTORY);
+        console.log("test", HistoryMock.MOCK_HISTORY);
 
         component.loadHistory().then(() => {
           expect(component.historyItemFilter.filter).toHaveBeenCalled();
--- src/app/services/trello-data.service.spec.ts
+++ src/app/services/trello-data.service.spec.ts
@@ -1,17 +1,41 @@
-import { TestBed } from '@angular/core/testing';
-
+import { async, TestBed } from '@angular/core/testing';
 import { TrelloDataService } from './trello-data.service';
 import { HttpClientModule } from '@angular/common/http';
+import { HistoryMock } from '../shared/models/history/history.model.mock';
 
 describe('TrelloDataService', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [
-      HttpClientModule
-    ]
-  }));
+  let service: TrelloDataService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientModule
+      ]
+    });
+
+    service = TestBed.get(TrelloDataService);
+  });
 
   it('should be created', () => {
-    const service: TrelloDataService = TestBed.get(TrelloDataService);
     expect(service).toBeTruthy();
+  });
+
+  describe('getRequestUri', () => {
+    it('should return the Trello API URI formulated with the provided short link', () => {
+      const actual = service.getRequestUri(HistoryMock.MOCK_SHORT_LINK);
+      const expected = `https://trello.com/1/cards/${HistoryMock.MOCK_SHORT_LINK}/actions?filter=createCard,convertToCardFromCheckItem,updateCard&limit=1000`;
+
+      expect(actual).toBe(expected);
+    });
+  });
+
+  describe('getHistory', async () => {
+    it('should get the history via the Trello API', () => {
+      service.getHistory(HistoryMock.MOCK_SHORT_LINK).subscribe((history) => {
+        expect(history).toBeTruthy();
+        expect(history.historyItems).toBeTruthy();
+        expect(history.historyItems.length).toBeGreaterThanOrEqual(HistoryMock.MOCK_HISTORY.historyItems.length);
+      });
+    });
   });
 });
