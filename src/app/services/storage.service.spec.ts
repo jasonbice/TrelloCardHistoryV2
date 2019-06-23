940a752bad9311d2ec52e8aa325db80db40456c0
Jason Bice <jason.bice@gmail.com>
2019-06-23 11:04:48 -0400

    Fixing storage service test

--- src/app/services/storage.service.spec.ts
+++ src/app/services/storage.service.spec.ts
@@ -1,9 +1,18 @@
 import { TestBed } from '@angular/core/testing';
 
 import { StorageService } from './storage.service';
+import { AppRoutingModule } from '../app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('StorageService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        AppRoutingModule,
+        HttpClientModule
+      ]
+    });
+  });
 
   it('should be created', () => {
     const service: StorageService = TestBed.get(StorageService);
